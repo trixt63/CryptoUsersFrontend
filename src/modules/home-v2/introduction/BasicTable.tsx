aae730974a77c07a370798e495dca0ab1ea1ed05
Trinh Xuan Tung <xuantung.trinh.63@gmail.com>
2023-07-03 17:46:39 +0700

    editing BasicTable

--- src/modules/home-v2/introduction/BasicTable.tsx
+++ src/modules/home-v2/introduction/BasicTable.tsx
@@ -6,29 +6,26 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { useHomeContext } from 'src/modules/home-v2/context';
 
-function createData(
-  name: string,
-  appsNumber: number,
-  usersNumber: number,
-  realUsersNumber: number,
-) {
-  return { name, calories: appsNumber, fat: usersNumber, carbs: realUsersNumber};
+function createData(name: string, appsNumber: number, usersNumber: number, realUsersNumber: number) {
+  return { name, calories: appsNumber, fat: usersNumber, carbs: realUsersNumber };
 }
 
-const rows = [
-  createData('CEX', 159, 6.0, 24),
-  createData('DEX', 237, 9.0, 37),
-  createData('Lendings', 262, 16.0, 24),
+const intro = [
+  createData('CEX', 12, 6.0, 24),
+  createData('DEX', 3, 9.0, 37),
+  createData('Lendings', 7, 16.0, 24),
 ];
 
 export default function BasicTable() {
+  // const { intro } = useHomeContext();
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell></TableCell>
+            <TableCell />
             <TableCell align="right">Number of applications</TableCell>
             <TableCell align="right">Number of users</TableCell>
             <TableCell align="right">Number of real users</TableCell>
@@ -36,7 +33,7 @@ export default function BasicTable() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {intro.map((row) => (
             <TableRow
               key={row.name}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
@@ -50,6 +47,8 @@ export default function BasicTable() {
               {/*<TableCell align="right">{row.protein}</TableCell>*/}
             </TableRow>
           ))}
+
+          {/*{Object.entries(intro)}*/}
         </TableBody>
       </Table>
     </TableContainer>
